c4d980a128b447b7a19c1bf42379892b7da65cbf
MFirtina61 <114576186+MFirtina61@users.noreply.github.com>
2022-10-12 12:29:22 +0200

    kleine fout

--- haiku.py
+++ haiku.py
@@ -2,13 +2,13 @@
 
 #plak hier de haiku functions onder elkaar
 
-def HaikuX(): 
+def Haiku1(): 
   print("haiku by:","AUTHOR") 
   print("Hallo mijn naam is mucahid") 
   print("Ik vind mijn opleding heel erg leuk") 
   print("ik zit in sd1a klas")
   
-def Haiku1(): 
+def Haiku2(): 
   print("haiku by:","AUTHOR") 
   print("Hallo mijn naam is mostafa") 
   print("Ik vind mijn opleding heel erg leuk") 
